57028a1e39f504648c6aa24fa8bb56c4f953b09a
Sathish Ganapathy <Sathish.Ganapathy@analance.com>
2017-05-09 18:28:27 +0530

    Reference code for angular 2 demo

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+//import component for routing 
+import { DashboardComponent } from "./dashboard.component";
+import { HeroesComponent } from "./heroes.component";
+import { HeroDetailComponent } from "./hero-detail.component";
+
+const routes: Routes = [
+{path:"",redirectTo:"/dashboard",pathMatch:"full"},
+{path : "dashboard", component : DashboardComponent},
+{path : "detail/:id", component : HeroDetailComponent},
+{path : "heroes", component : HeroesComponent }
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
